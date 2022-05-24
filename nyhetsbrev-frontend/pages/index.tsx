b7745aaa0c9a79d61791339c9d7d58e48f1549b5
André Narvell <andrenarvell@hotmail.com>
2022-05-24 14:45:08 +0200

    Day 2, working on conditional rendering

--- nyhetsbrev-frontend/pages/index.tsx
+++ nyhetsbrev-frontend/pages/index.tsx
@@ -1,8 +1,39 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import axios from 'axios'
+import { IUser } from '../models/IUser'
+import { ChangeEvent, useState } from 'react'
+import { ILogin } from '../models/ILogin'
 
 const Home: NextPage = () => {
+  const [failure, setFailure] = useState<any>('')
+  const [user, setUser] = useState<ILogin>({
+    email: '',
+    password: '',
+  })
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  const signinUser = async () => {
+    await axios.post('http://localhost:5000/users/login', user).then((res) => {
+      if (res.data == 'Failure') {
+        setFailure(<p>Fel användarnamn eller lösenord</p>)
+      } else {
+        localStorage.setItem('user', JSON.stringify(res.data))
+        setFailure('')
+      }
+    })
+  }
+
+  const handleClick = () => {
+    signinUser()
+  }
+
+  const handleLogin = (e: ChangeEvent<HTMLInputElement>) => {
+    setUser({ ...user, [e.target.name]: e.target.value })
+  }
+
   return (
+    // {!loggedIn && ()}
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <h1 className="mb-8 text-center text-3xl">Very nice log in page yes</h1>
       <form className="w-full max-w-sm">
@@ -18,6 +49,8 @@ const Home: NextPage = () => {
               id="inline-full-name"
               type="email"
               placeholder="Email"
+              name="email"
+              onChange={handleLogin}
             />
           </div>
         </div>
@@ -32,7 +65,9 @@ const Home: NextPage = () => {
               className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
               id="inline-password"
               type="password"
-              placeholder="********"
+              placeholder="••••••••"
+              name="password"
+              onChange={handleLogin}
             />
           </div>
         </div>
@@ -43,6 +78,7 @@ const Home: NextPage = () => {
             <button
               className="focus:shadow-outline mr-5 rounded bg-purple-500 py-2 px-4 font-bold text-white shadow hover:bg-purple-400 focus:outline-none"
               type="button"
+              onClick={handleClick}
             >
               Log in
             </button>
@@ -54,6 +90,7 @@ const Home: NextPage = () => {
                 Sign up
               </button>
             </Link>
+            {failure}
           </div>
         </div>
       </form>
--- nyhetsbrev-frontend/pages/loggedin.tsx
+++ nyhetsbrev-frontend/pages/loggedin.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 
 const Loggedin: NextPage = () => {
   return (
