ffdf6e8633e7b2fe8029f5df57cb3663f14665e5
André Narvell <andrenarvell@hotmail.com>
2022-05-25 17:02:11 +0200

    Almost done

--- nyhetsbrev-frontend/pages/index.tsx
+++ nyhetsbrev-frontend/pages/index.tsx
@@ -2,99 +2,191 @@ import type { NextPage } from 'next'
 import Link from 'next/link'
 import axios from 'axios'
 import { IUser } from '../models/IUser'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { ILogin } from '../models/ILogin'
 
 const Home: NextPage = () => {
   const [failure, setFailure] = useState<any>('')
+  const [changesSub, setChangedSub] = useState<any>('')
   const [user, setUser] = useState<ILogin>({
     email: '',
     password: '',
   })
+
   const [loggedIn, setLoggedIn] = useState(false)
+  const [checkbox, setCheckbox] = useState(false)
+
+  const [subUser, setSubUser] = useState({
+    userId: '',
+    subscription: !checkbox,
+  })
 
   const signinUser = async () => {
     await axios.post('http://localhost:5000/users/login', user).then((res) => {
       if (res.data == 'Failure') {
         setFailure(<p>Fel användarnamn eller lösenord</p>)
       } else {
-        localStorage.setItem('user', JSON.stringify(res.data))
+        localStorage.setItem('user', JSON.stringify(res.data.userPost.userId))
         setFailure('')
+        setLoggedIn(true)
+        setCheckbox(res.data.userPost.subscription)
+        setSubUser({ ...subUser, userId: res.data.userPost.userId })
+        console.log(res.data.userPost.subscription)
       }
     })
+  }
+
+  const updateSub = () => {
+    axios.put('http://localhost:5000/users/change', subUser).then((res) => {
+      console.log(res)
+    })
+    console.log(checkbox)
+  }
+
+  const handleChecked = () => {
+    setCheckbox(!checkbox)
+    setSubUser({ ...subUser, subscription: !checkbox })
+    setChangedSub(<p>Subscription status changed!</p>)
+
+    setTimeout(() => {
+      setChangedSub('')
+
+      console.log(!checkbox)
+    }, 3000)
   }
 
   const handleClick = () => {
     signinUser()
   }
 
+  const handleLogout = () => {
+    localStorage.clear()
+    setLoggedIn(false)
+  }
+
   const handleLogin = (e: ChangeEvent<HTMLInputElement>) => {
     setUser({ ...user, [e.target.name]: e.target.value })
   }
 
-  return (
-    // {!loggedIn && ()}
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <h1 className="mb-8 text-center text-3xl">Very nice log in page yes</h1>
-      <form className="w-full max-w-sm">
-        <div className="mb-6 md:flex md:items-center">
-          <div className="md:w-1/3">
-            <label className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0 md:text-right">
-              Email
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
-              id="inline-full-name"
-              type="email"
-              placeholder="Email"
-              name="email"
-              onChange={handleLogin}
-            />
-          </div>
-        </div>
-        <div className="mb-6 md:flex md:items-center">
-          <div className="md:w-1/3">
-            <label className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0 md:text-right">
-              Password
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
-              id="inline-password"
-              type="password"
-              placeholder="••••••••"
-              name="password"
-              onChange={handleLogin}
-            />
-          </div>
-        </div>
+  useEffect(() => {
+    if (localStorage.getItem('user') || '') {
+      setLoggedIn(true)
+      setCheckbox(subUser.subscription)
+    }
+  }, [])
 
-        <div className="md:flex md:items-center">
-          <div className="md:w-1/3"></div>
-          <div className="md:w-2/3">
-            <button
-              className="focus:shadow-outline mr-5 rounded bg-purple-500 py-2 px-4 font-bold text-white shadow hover:bg-purple-400 focus:outline-none"
-              type="button"
-              onClick={handleClick}
-            >
-              Log in
-            </button>
-            <Link href={'/signup'}>
-              <button
-                className="focus:shadow-outline rounded bg-purple-500 py-2 px-4 font-bold text-white shadow hover:bg-purple-400 focus:outline-none"
-                type="button"
-              >
-                Sign up
-              </button>
-            </Link>
-            {failure}
-          </div>
+  // let loggedInUser = localStorage.getItem('user') || ''
+
+  return (
+    <>
+      {!loggedIn && (
+        <div className="flex min-h-screen flex-col items-center justify-center py-2">
+          <h1 className="mb-8 text-center text-3xl">
+            Very nice log in page yes
+          </h1>
+          <form className="w-full max-w-sm">
+            <div className="mb-6 md:flex md:items-center">
+              <div className="md:w-1/3">
+                <label className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0 md:text-right">
+                  Email
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+                  id="inline-full-name"
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  onChange={handleLogin}
+                />
+              </div>
+            </div>
+            <div className="mb-6 md:flex md:items-center">
+              <div className="md:w-1/3">
+                <label className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0 md:text-right">
+                  Password
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+                  id="inline-password"
+                  type="password"
+                  placeholder="••••••••"
+                  name="password"
+                  onChange={handleLogin}
+                />
+              </div>
+            </div>
+
+            <div className="md:flex md:items-center">
+              <div className="md:w-1/3"></div>
+              <div className="md:w-2/3">
+                <button
+                  className="focus:shadow-outline mr-5 rounded bg-purple-500 py-2 px-4 font-bold text-white shadow hover:bg-purple-400 focus:outline-none"
+                  type="button"
+                  onClick={handleClick}
+                >
+                  Log in
+                </button>
+                <Link href={'/signup'}>
+                  <button
+                    className="focus:shadow-outline rounded bg-purple-500 py-2 px-4 font-bold text-white shadow hover:bg-purple-400 focus:outline-none"
+                    type="button"
+                  >
+                    Sign up
+                  </button>
+                </Link>
+                {failure}
+              </div>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
+      )}
+      {loggedIn && (
+        <div className="flex min-h-screen flex-col items-center justify-center py-2">
+          <h1 className="mb-5 text-9xl">😋</h1>
+          <h1 className="mb-8 text-center text-3xl">
+            Welcome user with ID: <br /> {localStorage.getItem('user')}
+          </h1>
+
+          <p className="text-l mb-5 text-center">
+            Would you like to change your subscription status?
+          </p>
+          <form
+            action="submit"
+            className="flex w-full max-w-sm flex-col items-center justify-center "
+          >
+            <input
+              type="checkbox"
+              className="h-6 w-6  rounded-md border-0 focus:ring-0"
+              onChange={handleChecked}
+              checked={checkbox}
+            />
+            {changesSub}
+
+            <br />
+            <button
+              type="button"
+              onClick={updateSub}
+              className="focus:shadow-outline rounded bg-purple-500 py-2 px-4 font-bold text-white shadow hover:bg-purple-400 focus:outline-none"
+            >
+              Save
+            </button>
+          </form>
+          <Link href={'/'}>
+            <button
+              className="focus:shadow-outline mt-5 rounded bg-purple-500 py-2 px-4 font-bold text-white shadow hover:bg-purple-400 focus:outline-none"
+              type="button"
+              onClick={handleLogout}
+            >
+              Log out
+            </button>
+          </Link>
+        </div>
+      )}
+    </>
   )
 }
 
